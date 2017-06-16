1f76ba64bfb3e37445abc1e8fb7c3785fdb6b077
Ivan Agarkov <i_agarkov@wargaming.net>
2017-06-16 16:33:50 +0300

    ldap plugin

--- plugins/ldap/ldapauth.go
+++ plugins/ldap/ldapauth.go
@@ -1,19 +1,74 @@
 package ldap
 
 import (
-	"github.com/jtblin/go-ldap-client"
 	"net/http"
+	"strconv"
+	"gopkg.in/ldap.v2"
+	"fmt"
+	"crypto/tls"
+	"log"
+)
+
+const (
+	BASE = "ldap_base"
+	HOST = "ldap_host"
+	PORT = "ldap_port"
+	SSL  = "ldap_use_ssl"
 )
 
 type LdapPlugin struct {
-	config map[string]string
-	client ldap.LDAPClient
+	base string
+	host string
+	port int
+	ssl  bool
 }
 
 func (l *LdapPlugin) RequestIntercept(w http.ResponseWriter, r *http.Request) bool {
+	username, password, ok := r.BasicAuth()
+	if ok {
+		ok = false
+		var conn *ldap.Conn
+		var e error
+		address := fmt.Sprintf("%s:%d", l.host, l.port)
+		if l.ssl {
+			config := &tls.Config{
+				InsecureSkipVerify: true,
+			}
+			conn, e = ldap.DialTLS("tcp", address, config)
+		} else {
+			conn, e = ldap.Dial("tcp", address)
+		}
+		if e == nil {
+			defer conn.Close()
+			if conn.Bind(username, password) == nil {
+				ok = true
+			} else {
+				log.Println("Auth failed for", username)
+			}
+		} else {
+			log.Println("Something went wrong with ldap:", e.Error())
+		}
+	}
+	if !ok {
+		if !ok {
+			w.Header().Add("WWW-Authenticate", `Basic realm="ActiveDirectory auth"`)
+			http.Error(w, "Authorization required", http.StatusUnauthorized)
+			return true
+		}
+	}
 	return false
 }
 
 func Init(config map[string]string) *LdapPlugin {
-	return nil
+	l := new(LdapPlugin)
+	port, _ := strconv.Atoi(config[PORT])
+	ssl := true
+	if config[SSL] == "false" {
+		ssl = false
+	}
+	l.base = config[BASE]
+	l.port = port
+	l.host = config[HOST]
+	l.ssl = ssl
+	return l
 }
